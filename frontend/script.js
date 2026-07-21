1446155c76b929f8d27483f4f2df2ae2c6846c68
kumaresh-Analyst <kumaresh.analyst@gmail.com>
2026-07-21 14:34:07 +0530

    Fixed Lambda integration

--- frontend/script.js
+++ frontend/script.js
@@ -1,63 +1,21 @@
- async function checkBackend() {
+async function checkBackend() {
 
     const url = "https://j5f6wdfwxojtzaix3o5u2ap2my0ddfwu.lambda-url.us-east-1.on.aws/";
 
     try {
 
         const response = await fetch(url);
+
         const data = await response.json();
 
-        document.getElementById("status").innerHTML = data.body;
+        document.getElementById("status").innerHTML = data.message;
 
     } catch (error) {
 
-        document.getElementById("status").innerHTML =
-            "Backend connection failed";
-
         console.error(error);
+
+        document.getElementById("status").innerHTML = error.message;
 
     }
 
-}
-
-function loadEmployees() {
-
-    let employees = [
-        "Kumaresh",
-        "Prem",
-        "Likhith"
-    ];
-
-    let list = document.getElementById("employees");
-
-    list.innerHTML = "";
-
-    employees.forEach(employee => {
-
-        let li = document.createElement("li");
-        li.textContent = employee;
-        list.appendChild(li);
-
-    });
-}
-
-function loadProducts() {
-
-    let products = [
-        "Laptop",
-        "Mouse",
-        "Keyboard"
-    ];
-
-    let list = document.getElementById("products");
-
-    list.innerHTML = "";
-
-    products.forEach(product => {
-
-        let li = document.createElement("li");
-        li.textContent = product;
-        list.appendChild(li);
-
-    });
 }
